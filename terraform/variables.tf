4d536d72b3183f8a491acf6382f3d78949003216
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-06-18 19:31:35 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,29 @@
-variable "region" {
-  default = "us-east-1"
-}
-
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
-  default = ["10.0.11.0/24", "10.0.12.0/24"]
-}
-
-variable "instance_type" {
-  default = "t2.micro"
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "ami" {
-  default = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 in us-east-1
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-09e6f87a47903347c" 
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
 }
