3715365a40e152eafbca58fcf3041434ddcbf2ae
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-06-18 18:32:48 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+#VPC and Subnets
+
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
@@ -21,4 +23,104 @@ resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+#Route tables and NAT Gateway
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+#Security Group
+
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2_sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#EC2 Instance in private subnet
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.private[0].id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = false
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y java-1.8.0-openjdk nginx wget unzip
+
+              cd /opt
+              wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
+              tar -xzf apache-tomcat-9.0.85.tar.gz
+              mv apache-tomcat-9.0.85 tomcat9
+              chmod +x tomcat9/bin/*.sh
+              nohup tomcat9/bin/startup.sh &
+
+              systemctl start nginx
+              systemctl enable nginx
+              echo "<h1>Welcome to Nginx + Tomcat on EC2</h1>" > /usr/share/nginx/html/index.html
+              EOF
+
+  tags = {
+    Name = "TomcatNginxEC2"
+  }
 }
