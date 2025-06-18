c8c6f3f9b4b0801bd43cdcd244e72233f7ddb4f7
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-06-18 23:17:56 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,6 +100,65 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# Security Group for RDS
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow EC2 instances to access PostgreSQL"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id] # EC2 SG access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
+  }
+}
+
+# DB Subnet Group using the private subnets
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [for subnet in aws_subnet.private : subnet.id]
+
+  tags = {
+    Name = "rds-subnet-group"
+  }
+}
+
+# RDS PostgreSQL Instance
+resource "aws_db_instance" "postgres" {
+  identifier              = "my-postgres-db"
+  allocated_storage       = var.db_allocated_storage
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  engine_version          = "15"
+  instance_class          = var.db_instance_class
+  db_name                 = var.db_name
+  username                = var.db_username
+  password                = var.db_password
+  publicly_accessible     = false
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+  multi_az                = false
+  deletion_protection     = false
+
+  tags = {
+    Name = "PrivateRDSPostgres"
+  }
+}
+
+
 # EC2 Instance in Private Subnet
 resource "aws_instance" "web" {
   ami                         = var.ami
