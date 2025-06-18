8e0a89b7c35d310954f2a3444bdeff36e563cac3
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-06-18 23:18:35 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,3 +33,35 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "db_name" {
+  description = "The name of the RDS database"
+  type        = string
+  default     = "mydb"
+}
+
+variable "db_username" {
+  description = "Master username for the RDS database"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Master password for the RDS database"
+  type        = string
+  default     = "admin12345"
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "RDS instance type"
+  type        = string
+  default     = "db.t3.micro" 
+}
+
+variable "db_allocated_storage" {
+  description = "Storage size in GB for RDS"
+  type        = number
+  default     = 20
+}
+
