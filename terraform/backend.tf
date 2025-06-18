3b9e662f0565c242642da8180c0066c564c0a126
pmlktrn <66727828+pmlktrn@users.noreply.github.com>
2025-06-18 20:52:35 +0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket-pam"
     key            = "terraform.tfstate"
     region         = "ap-southeast-1"
     encrypt        = true
